317afdf76878c5296112f5e06693c0eb065ed563
arshey_mishra <mishraarshey@gmail.com>
2025-05-18 18:27:45 +0530

    Clean up unnecessary comments in Kafka configuration

--- src/config/kafka.js
+++ src/config/kafka.js
@@ -1,13 +1,8 @@
-// ES Module compatibility with CommonJS modules
 import kafkaNode from 'kafka-node';
 
-// In ES modules, we need to access the default export for CommonJS modules
 const { KafkaClient, Producer, Consumer } = kafkaNode;
 
-// Create Kafka client
 const client = new KafkaClient({ kafkaHost: process.env.KAFKA_BROKER });
-
-// Create producer and consumer
 const producer = new Producer(client);
 const consumer = new Consumer(
   client,
@@ -15,7 +10,6 @@ const consumer = new Consumer(
   { autoCommit: true }
 );
 
-// Handle connection events
 producer.on('ready', () => {
   console.log('Kafka producer is ready');
 });
